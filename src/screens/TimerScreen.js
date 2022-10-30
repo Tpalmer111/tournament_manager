3dfbfb127c69015390627d75ee0ee0d6980ba766
Tpalmer111 <mr.palmer111@gmail.com>
2022-10-30 09:43:52 -0600

    edited TimerScreen, basic levels count up and timer restarts

--- src/screens/TimerScreen.js
+++ src/screens/TimerScreen.js
@@ -10,81 +10,103 @@ import {
 import { Ionicons } from "@expo/vector-icons";
 // import CountDown from 'react-native-countdown-component';
 
+const c = console.log.bind(document)
 
 
 export default function ({ navigation }) {
-  const { isDarkmode } = useTheme();
-  let smallBlind = 25;
-  let bigBlind = 50;
-  let level = 1;
-  let isRunning = true
-  
-  
-  const timerSwitch = () => {
-    isRunning = !isRunning
-  }
-  
-  const blindLevel = () => {
-    // variables to track go here
-    let time = (60 * 15)
-    // timer function
-    if (!isRunning) {
-        return
-    } else if (isRunning) {
-        setInterval(callback, 1000);
+    const { isDarkmode } = useTheme();
+    // const [timer, setTimer] = useState(0)
+    let smallBlind = 25;
+    let bigBlind = 50;
+    let level = 1;
+    let isRunning = false
+    // let displayTime = 0
+    let minutes = 0;
+    let seconds = 0;
+    let time = (5 * 1)
+    
+    let interval
 
-        function callback() {
-            time--
-            console.log(`minutes: ${Math.floor(time / 60)} and seconds: ${Math.floor(time % 60)}`)
-        } 
+    // useEffect(() => {
+    //     const timerStartInterval = setInterval(() => {
+    //         setTimer((time) => time - 1);
+    //     }, 1000);
+    //     return () => {
+    //         clearInterval(timerStartInterval);
+    //     }
+    // }, []);
+  
+
+    const timerSwitch = () => {
+        isRunning = !isRunning
+        if (isRunning) {
+            // timer function
+            interval = setInterval(callback, 1000);
+            c(isRunning)
+            // c(`minutes: ${minutes} and seconds: ${seconds}`)
+        } else if (!isRunning) {
+            clearInterval(interval)
+            c(isRunning)
+        }
     }
 
-    
-    // call a function to restart clock
-      // plus one to the level
-      level = level + 1
-      // multiply the blinds by 2x
-      smallBlind = smallBlind * 2
-      bigBlind = bigBlind * 2
-      return
-
-
-  }
-
-  return (
-    <Layout>
-      <TopNav
-        middleContent="Tournament Page"
-        leftContent={
-          <Ionicons
-            name="chevron-back"
-            size={20}
-            color={isDarkmode ? themeColor.white100 : "#191921"}
-          />
+    function callback() {
+        time--
+        minutes = Math.floor(time / 60)
+        seconds = Math.floor(time % 60)
+        c(`${minutes}:${seconds}`)
+        if (minutes === 0 && seconds === 0) {
+            blindLevel()
+            c(level)
+        } else {
+            return
         }
-        leftAction={() => navigation.goBack()}
-      />
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        
-        <Text 
-          style={{
-            fontSize: 45
-          }}
+    }
+    
+    const blindLevel = () => {
+        // variables to track go here
+        // call a function to restart clock
+        // plus one to the level
+        level = level + 1
+        // multiply the blinds by 2x
+        smallBlind = smallBlind * 2
+        bigBlind = bigBlind * 2
+        time = (5 * 1)
+        return
+    }
 
-          >Level {level}</Text>
-        <Button title="start/pause" onPress={timerSwitch} />
-        <View>
-            <Text>{blindLevel()}</Text>
-        </View>
-        <Text fontWeight="bold">Blinds: {smallBlind}/{bigBlind}</Text>
-        <Text>Break: 60:00</Text>
-      </View>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <TopNav
+                middleContent="Tournament Page"
+                leftContent={
+                    <Ionicons
+                        name="chevron-back"
+                        size={20}
+                        color={isDarkmode ? themeColor.white100 : "#191921"}
+                    />
+                }
+                leftAction={() => navigation.goBack()}
+            />
+            <View
+                style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+                }}
+            >
+                <Text 
+                    style={{
+                    fontSize: 45
+                    }}
+                >Level {level}</Text>
+            <Button title="start/pause" onPress={timerSwitch} />
+            <View>
+                <Text>{}</Text>
+            </View>
+                <Text fontWeight="bold">Blinds: {smallBlind}/{bigBlind}</Text>
+                <Text>Break: 60:00</Text>
+            </View>
+        </Layout>
+    );
 }
